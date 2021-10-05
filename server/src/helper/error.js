e14c9dcbf4ec2a8de3b0fbd30466829df139460e
NazrulChowdhury <nazrulchowdhury404@gmail.com>
2021-10-05 18:15:16 +0100

    Custom Error Class added

---
+++ server/src/helper/error.js
@@ -0,0 +1,16 @@
+class ApiError {
+    constructor(code, message, fields){
+        this.code = code
+        this.message = message
+        this.fields = fields
+    }
+
+    static badRequest(code, message, fields){
+        return new ApiError(code, message, fields)
+    }
+
+    static internalError(){
+        return new ApiError('500', 'opps! something went wrong!')
+    }
+}
+module.exports = ApiError
