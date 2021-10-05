76161df088a1d8f94f9b17163a92e282e2be61a0
NazrulChowdhury <nazrulchowdhury404@gmail.com>
2021-10-05 18:24:48 +0100

    error middleare added to the route

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
  const authenticate = require('../middleware/authenticate.middleware')
+const errorHandler = require('../middleware/error.middleware')
 
 
 router.get('/', require('../controllers/auth.Controller'))
 router.post('/addUser')
 router.use('/auth', require('./auth.route'))
+router.use(errorHandler)
 
 module.exports = router
