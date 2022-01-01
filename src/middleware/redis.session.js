faf54af6aae384c7198b77b0646b33b8f69fda88
NazrulChowdhury <nazrulchowdhury404@gmail.com>
2022-01-01 16:48:04 +0000

    first try

--- src/middleware/redis.session.js
+++ src/middleware/redis.session.js
@@ -6,10 +6,8 @@ require('dotenv').config()
 
 const RedisStore = connectRedis(session)
 //Configure redis client
-const redisClient = redis.createClient({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT  
-})
+const redisClient = redis.createClient({url: process.env.REDIS_URL})
+
 redisClient.on('error', function (err) {
     console.log('Could not establish a connection with redis. ' + err)
 })
