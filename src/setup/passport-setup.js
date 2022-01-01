de2fa0f858e4fcb4ef326dcb172b91a322518f1f
NazrulChowdhury <nazrulchowdhury404@gmail.com>
2022-01-01 22:55:53 +0000

    added proxy true in google strategy

--- src/setup/passport-setup.js
+++ src/setup/passport-setup.js
@@ -17,7 +17,8 @@ passport.use(new GoogleStrategy({
     // options for google strategy
     clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
     clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
-    callbackURL: '/auth/google/redirect'
+    callbackURL: 'https://enigmatic-river-04425.herokuapp.com/auth/google/redirect',
+    proxy: true
   }, 
   async(request, accessToken, refreshToken, profile, done)=>{
     try{
