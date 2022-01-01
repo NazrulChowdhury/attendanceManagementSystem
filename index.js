8be3c07ad86a7db776435d46b59e45be2b3fc704
NazrulChowdhury <nazrulchowdhury404@gmail.com>
2022-01-01 18:31:27 +0000

    modified

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ app.use('/api',router)
 db()
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'))
+    app.use(express.static(path.join( __dirname, 'client', 'build')))
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
